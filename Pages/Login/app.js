47a8de3e5731e23ec09517ea724a9060b6f69d5b
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-07-26 20:41:37 +0430

    [ADD] tastfully handled bg transition

--- Pages/Login/app.js
+++ Pages/Login/app.js
@@ -2,9 +2,29 @@ const loginForm = document.querySelector(".login-form");
 const signForm = document.querySelector(".signup-form");
 const formWrapper = document.querySelector(".form-wrapper");
 const alreadyHaveAnAccountCta = document.querySelector("#already-have-account");
+const createNewAccountCta = document.querySelector("#create-new-account");
+const main = document.getElementsByTagName("main");
+
+createNewAccountCta.addEventListener("click", () => {
+  formPosChanHandler();
+
+  main[0].className = "bg-animation-2";
+
+  setTimeout(() => {
+    main[0].className = "bg-animation-3";
+  }, 400);
+});
 
 alreadyHaveAnAccountCta.addEventListener("click", () => {
+  formPosChanHandler();
+  main[0].className = "bg-animation-4";
+  setTimeout(() => {
+    main[0].className = "bg-animation-1";
+  }, 400);
+});
+
+const formPosChanHandler = () => {
   loginForm.classList.toggle("display-none");
   signForm.classList.toggle("display-none");
   formWrapper.classList.toggle("signup-form-wrapper");
-});
+};
