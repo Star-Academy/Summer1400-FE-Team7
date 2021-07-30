4e74bd7647198e4a4395aeea3b91e19280ae6b1a
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-07-30 22:52:52 +0430

    [ADD] layout change on radio btn click

--- pages/dashboard/styles.js
+++ pages/dashboard/styles.js
@@ -1,6 +1,9 @@
 // //VARIABLES DECLARATIONS
 const menuButtons = document.querySelectorAll(".btn-wrapper");
 const favIcon = document.querySelectorAll(".fav-icon");
+const munuOpenerCloser = document.querySelector("#side-menu-opener-closer-btn");
+const sideMenu = document.querySelector(".side-menu");
+const layoutBtns = document.querySelectorAll(".radio-btn");
 
 const menuClassOmmiter = () => {
   for (let m of menuButtons) {
@@ -20,3 +23,37 @@ const handelmenuButtonsSelect = () => {
 };
 
 handelmenuButtonsSelect();
+
+munuOpenerCloser.addEventListener("click", () => {
+  sideMenu.classList.toggle("side-menu-closed");
+  songList.classList.toggle("side-menu-opended");
+});
+
+layoutBtns.forEach((layout) => {
+  layout.addEventListener("change", (event) => {
+    switch (event.target.id) {
+      case "list-compact-view":
+        if (songList.classList.contains("grid-view")) {
+          songList.classList.remove("grid-view");
+        }
+        songList.classList.add("list-compact-view");
+        break;
+
+      case "grid-view":
+        if (songList.classList.contains("list-compact-view")) {
+          songList.classList.remove("list-compact-view");
+        }
+        songList.classList.add("grid-view");
+        break;
+
+      default:
+        if (songList.classList.contains("grid-view")) {
+          songList.classList.remove("grid-view");
+        }
+        if (songList.classList.contains("list-compact-view")) {
+          songList.classList.remove("list-compact-view");
+        }
+        break;
+    }
+  });
+});
