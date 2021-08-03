5b33803a8903ef8d1d018978b6dd85a8a380c5aa
milad nafar <milarnafar78+glab@gmail.com>
2021-08-03 21:29:17 +0430

    [ADD] add email validation into login

--- source/auth/login.js
+++ source/auth/login.js
@@ -2,11 +2,17 @@ const loginForm = document.querySelector(".login-form");
 const emailLoginInput = document.querySelector("#login-email-input");
 const passwordLoginInput = document.querySelector("#login-password-input");
 const loginBtn = document.querySelector(".login-btn");
-const emailInputWrapper = document.querySelector("#email-input-wrapper");
+const emailInputWrapper = document.querySelector("#email-login-input-wrapper");
+const passwordInputWrapper = document.querySelector("#password-login-input-wrapper");
 
 const loginInputs = {
   email: "",
   password: "",
+};
+const validateEmail = (email) => {
+  const emailPattern =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailPattern.test(String(email).toLowerCase());
 };
 
 const performLogin = () => {
@@ -16,18 +22,43 @@ const performLogin = () => {
 
   if (loginInputs.email == "") {
     console.log("email cant be empty");
+    emailInputWrapper.setAttribute("data-error", "ایمیل نمی‌تواند خالی باشد");
     emailInputWrapper.classList.add("error");
     return false;
+  }
+  else {
+    emailInputWrapper.classList.remove("error");
   }
 
   if (loginInputs.password == "") {
     console.log("password cant be empty");
+    passwordInputWrapper.setAttribute("data-error", "رمزعبور نمی‌تواند خالی باشد");
+
+    passwordInputWrapper.classList.add("error");
+
+
     return false;
+  } else{
+    passwordInputWrapper.classList.remove("error");
+
+  }
+
+  if (!validateEmail(loginInputs.email)) {
+    console.log("email is not valis");
+    emailInputWrapper.setAttribute("data-error", "ایمیل وارد شده معتبر نیست!");
+    emailInputWrapper.classList.add("error");
+
+    return false;
+  } else {
+    emailInputWrapper.classList.remove("error");
   }
 
   let result = false;
   for (const user of testUsers) {
-    if (user.username == loginInputs.email.split("@")[0] && user.passowrd == loginInputs.password) {
+    if (
+      user.username == loginInputs.email.split("@")[0] &&
+      user.passowrd == loginInputs.password
+    ) {
       console.log("login seccussed");
       result = true;
       break;
--- source/auth/register.js
+++ source/auth/register.js
@@ -10,11 +10,6 @@ const registerInputs = {
   password: "",
   confirmPassword: "",
 };
-const validateEmail = (email) => {
-  const emailPattern =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailPattern.test(String(email).toLowerCase());
-};
 
 const validPassword = (password) => {
   const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
