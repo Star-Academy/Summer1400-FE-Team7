ad97049611c7c5dab7ec593659423f491cd1d6f6
milad nafar <milarnafar78+glab@gmail.com>
2021-08-03 11:05:00 +0430

    [FIX] fix a tag id

--- pages/dashboard/musicControllers.js
+++ pages/dashboard/musicControllers.js
@@ -1,7 +1,12 @@
 const playBtn = document.querySelectorAll(".play-music-btn");
 const pauseBtnImg = "../../assets/images/controls/pause.svg";
 const playBtnImg = "../../assets/images/controls/play-button.svg";
+
 const audio = document.querySelector("#audio");
+const nextBtn = document.querySelector("#next-btn");
+const previousBtn = document.querySelector("#previous-btn");
+const currentTimeLabel = document.querySelector("#current-time");
+const endTimeLabel = document.querySelector("#end-time");
 
 const statusTypes = {
   PLAYING: "playing",
@@ -11,55 +16,115 @@ const statusTypes = {
 };
 let status = statusTypes.STOPED;
 
+const loadMusic = () => {
+  audio.src = songs[currentMusicIndex].file;
+  const songname = `name :${songs[currentMusicIndex].name}`;
+  console.log("🚀loadMusic", songname);
+  seekSlider.max = audio.duration;
+  console.log(audio.duration)
+  endTimeLabel.innerHTML=convertHMS( audio.duration)
+
+};
+
+const nextMusic = () => {
+  currentMusicIndex++;
+  if (currentMusicIndex > songs.length - 1) {
+    currentMusicIndex = 0;
+  }
+  loadMusic();
+  play();
+};
+const previousMusic = () => {
+  currentMusicIndex--;
+  if (currentMusicIndex < 0) {
+    currentMusicIndex = songs.length - 1;
+  }
+  loadMusic();
+  play();
+};
+
 playBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     if (status == statusTypes.PLAYING) {
       pause();
     } else if (status == statusTypes.PASUED || status == statusTypes.STOPED) {
-      play();
+      resume();
     }
   });
 });
 
 const play = () => {
-  console.log("🚀 ~ file: musicControllers.js ~ line 33 ~ play ~ play");
+  console.log("🚀  play");
+  loadMusic();
   playBtn.forEach((btn) => {
     btn.children[0].src = pauseBtnImg;
     btn.setAttribute("data-tooltip", "توقف");
   });
 
-    audio.addEventListener("canplaythrough", (event) => {
-      status = statusTypes.PLAYING;
+  audio.addEventListener("canplaythrough", (event) => {
+    status = statusTypes.PLAYING;
     audio.play();
   });
 };
 
 const resume = () => {
-  // music.r;
+  audio.play();
+  playBtn.forEach((btn) => {
+    btn.children[0].src = pauseBtnImg;
+    btn.setAttribute("data-tooltip", "توقف");
+  });
+  status = statusTypes.PLAYING;
+
 };
 
+const playBtnState=()=>{
+
+}
+
 const pause = () => {
-  console.log("🚀 ~ file: musicControllers.js ~ line 46 ~ pause ~ pause");
+  console.log("🚀 pause");
   audio.pause();
   status = statusTypes.PASUED;
   playBtn.forEach((btn) => {
     btn.children[0].src = playBtnImg;
     btn.setAttribute("data-tooltip", "پخش");
-
   });
 };
 
 const updateMusicBarValue = (value) => {
-  audio.currentTime=value;
-}
-audio.addEventListener("timeupdate",(event) => {
-
-  console.log(audio.currentTime)
-  seekSlider.max=audio.duration
-  seekSlider.value=audio.currentTime
+  audio.currentTime = value;
+  resume();
+  
+};
+audio.addEventListener("timeupdate", (event) => {
+  seekSlider.value = audio.currentTime;
+  currentTimeLabel.innerHTML=convertHMS( seekSlider.value )
 });
 
-audio.addEventListener("ended",(event) => {
+audio.addEventListener("ended", (event) => {
+  nextMusic();
+});
 
-})
+nextBtn.addEventListener("click", (event) => {
+  nextMusic();
+});
+previousBtn.addEventListener("click", (event) => {
+  previousMusic();
+});
 
+const doubleClickHandler = (elem, id) => {
+  elem.addEventListener("dblclick", () => {
+    const imgSrc = songs[id - 1].cover;
+    const elemTitle = songs[id - 1].name;
+    const elemArtist = songs[id - 1].artist;
+    bgCover.style.background = `url(${imgSrc}) center no-repeat`;
+    bgCover.style.backgroundSize = "cover";
+
+    musicCover.src = imgSrc;
+    musicArtist.innerText = elemArtist;
+    musicTitle.innerText = elemTitle;
+    currentMusicIndex = id - 1;
+
+    play();
+  });
+};
