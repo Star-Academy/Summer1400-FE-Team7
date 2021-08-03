e0d6d96f6d95ae98184cdc686318e9bdb90bfd22
milad nafar <milarnafar78+glab@gmail.com>
2021-08-03 18:48:40 +0430

    [ADD] add login

--- source/auth/login.js
+++ source/auth/login.js
@@ -1,7 +1,38 @@
-// const loginForm = document.querySelector(".login-form");
-// const emailLogin = document.querySelector("#login-email-input");
-// const passwordLogin = document.querySelector("#login-password-input");
-// const loginBtn = document.querySelector(".login-btn");
+const loginForm = document.querySelector(".login-form");
+const emailLoginInput = document.querySelector("#login-email-input");
+const passwordLoginInput = document.querySelector("#login-password-input");
+const loginBtn = document.querySelector(".login-btn");
+
+const loginInput = {
+  email: "",
+  password: "",
+};
+
+const performLogin = () => {
+  loginInput.email = emailLoginInput.value;
+  loginInput.password = passwordLoginInput.value;
+
+  const result = testUsers.filter((user) => {
+    if (user.username == loginInput.email.split("@")[0]) {
+      return true;
+    }
+    return false;
+  });
+  console.log(result);
+  return false;
+};
+
+const loginUser = (e) => {
+  if (performLogin()) {
+    window.location.replace("../../pages/dashboard/index.html");
+  }
+};
+
+// loginBtn.addEventListener("click",()=>{
+// })
+// loginForm.addEventListener("submit", () => {
+//   getLoginForm();
+// });
 
 // const login = async () => {
 //   user.email = emailLogin.value;
