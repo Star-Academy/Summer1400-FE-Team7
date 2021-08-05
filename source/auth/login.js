642f681e34d5631371fa29765f69bba74d870e8b
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-05 13:40:41 +0430

    [ADD] login mechanism

--- source/auth/login.js
+++ source/auth/login.js
@@ -1,78 +1,9 @@
-const loginForm = document.querySelector(".login-form");
-const emailLoginInput = document.querySelector("#login-email-input");
-const passwordLoginInput = document.querySelector("#login-password-input");
-const loginBtn = document.querySelector("#login-submit-btn");
-const emailInputWrapper = document.querySelector("#email-login-input-wrapper");
-const passwordInputWrapper = document.querySelector("#password-login-input-wrapper");
-
 const loginInputs = {
   email: "",
   password: "",
 };
 
-let isEmailLoginValid = false;
-let isPasswordLoginValid = false;
-let isFirstLoginSubmit = true;
-const validateEmail = (email) => {
-  const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailPattern.test(String(email).toLowerCase());
-};
-
-const checkLoginEmail = () => {
-  if (emailLoginInput.value == "") {
-    errorGenerator(emailInputWrapper, ERROR_MSG.MSG_1, ERROR_TYPES.TYPE_1);
-    return false;
-  } else if (!validateEmail(emailLoginInput.value)) {
-    errorGenerator(emailInputWrapper, ERROR_MSG.MSG_2, ERROR_TYPES.TYPE_1);
-
-    return false;
-  } else {
-    classRemover(emailInputWrapper, ERROR_TYPES.TYPE_1);
-    return true;
-  }
-};
-
-emailLoginInput.addEventListener("input", () => {
-  if (emailLoginInput.value == "") {
-    isEmailLoginValid = false;
-    errorGenerator(emailInputWrapper, ERROR_MSG.MSG_1, ERROR_TYPES.TYPE_2);
-  } else if (!validateEmail(emailLoginInput.value)) {
-    errorGenerator(emailInputWrapper, ERROR_MSG.MSG_2, ERROR_TYPES.TYPE_2);
-    isEmailLoginValid = false;
-  } else {
-    classRemover(emailInputWrapper, ERROR_TYPES.TYPE_1);
-    classRemover(emailInputWrapper, ERROR_TYPES.TYPE_2);
-    isEmailLoginValid = true;
-  }
-});
-
-const checkLoginPasswords = () => {
-  if (passwordLoginInput.value == "") {
-    errorGenerator(passwordInputWrapper, ERROR_MSG.MSG_3, ERROR_TYPES.TYPE_1);
-    return false;
-  } else {
-    classRemover(passwordInputWrapper, ERROR_TYPES.TYPE_1);
-    return true;
-  }
-};
-
-passwordLoginInput.addEventListener("input", () => {
-  if (passwordLoginInput.value == "") {
-    errorGenerator(passwordInputWrapper, ERROR_MSG.MSG_3, ERROR_TYPES.TYPE_2);
-    isPasswordLoginValid = false;
-  } else {
-    classRemover(passwordInputWrapper, ERROR_TYPES.TYPE_2);
-    isPasswordLoginValid = true;
-  }
-});
-
-const resetLoginForm = () => {
-  classRemover(loginBtn, ERROR_TYPES.TYPE_1);
-  emailLoginInput.value = "";
-  passwordLoginInput.value = "";
-};
-
-const performLogin = () => {
+const performLogin = async () => {
   loginInputs.email = emailLoginInput.value;
   loginInputs.password = passwordLoginInput.value;
 
@@ -82,28 +13,24 @@ const performLogin = () => {
   if (!isEmailLoginValid) return false;
   if (!isPasswordLoginValid) return false;
 
-  let result = false;
-  for (const user of testUsers) {
-    if (user.username == loginInputs.email.split("@")[0] && user.passowrd == loginInputs.password) {
-      result = true;
-      break;
-    }
-  }
+  const body = JSON.stringify(loginInputs);
 
-  if (!result) {
-    errorGenerator(loginBtn, ERROR_MSG.MSG_9, ERROR_TYPES.TYPE_1);
-  } else {
+  let response = await fetchInterceptor(USER_LOGIN_URI, METHOD_POST, body);
+  console.log("🚀 ~ file: login.js ~ line 19 ~ performLogin ~ response", response);
+
+  if (response.ok) {
+    const responseBody = await response.json();
+    localStorage.setItem("email", loginInputs.email);
+    localStorage.setItem("id", responseBody.id);
+    localStorage.setItem("token", responseBody.token);
+    window.location.href = "../../pages/dashboard/index.html";
     resetLoginForm();
+  } else {
+    errorGenerator(loginBtn, ERROR_MSG.MSG_9, ERROR_TYPES.TYPE_1);
   }
-
-  return result;
 };
 
 loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  if (performLogin()) {
-    localStorage.setItem("email", loginInputs.email);
-    loginForm.submit();
-    window.location.href = "../../pages/dashboard/index.html";
-  }
+  performLogin();
 });
