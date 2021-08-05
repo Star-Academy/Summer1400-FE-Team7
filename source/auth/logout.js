36f6d4abad72f83c99081444fae85218818290b8
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-05 13:39:59 +0430

    [ADD] logout mechanism

--- source/auth/logout.js
+++ source/auth/logout.js
@@ -1,4 +1,6 @@
 const logoutUser = () => {
   localStorage.removeItem("email");
+  localStorage.removeItem("id");
+  localStorage.removeItem("token");
   window.location.href = "../../pages/landing/index.html";
 };
