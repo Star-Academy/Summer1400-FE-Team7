24ae66952276808068ce568afdbabf171c66dae4
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-05 13:40:17 +0430

    [ADD] more endpoints

--- source/utils/env.js
+++ source/utils/env.js
@@ -1,1 +1,6 @@
 const BASE_URL = "http://130.185.120.192:5000";
+const METHOD_GET = "GET";
+const METHOD_POST = "POST";
+const USER_REGISTER_URI = "user/register";
+const USER_LOGIN_URI = "user/login";
+const SONG_PAGE_URI = "song/page";
