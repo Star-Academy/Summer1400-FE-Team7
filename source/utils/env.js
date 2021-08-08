7cc61efa0acc175d680cd581ace531fa7b108c2e
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-08 18:59:31 +0430

    [FIX] base url

--- source/utils/env.js
+++ source/utils/env.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://130.185.120.192:5000";
+const BASE_URL = " https://songs.code-star.ir/api-docs";
 const METHOD_GET = "GET";
 const METHOD_POST = "POST";
 const USER_REGISTER_URI = "user/register";
