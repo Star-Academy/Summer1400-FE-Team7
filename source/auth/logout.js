fe2593aa9b25d5e0e86362b2c68e0c2f817068fc
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-11 11:42:06 +0430

    [INIT] initializing angular project

--- source/auth/logout.js
+++
@@ -1,6 +0,0 @@
-const logoutUser = () => {
-  localStorage.removeItem("email");
-  localStorage.removeItem("id");
-  localStorage.removeItem("token");
-  window.location.href = "../../pages/landing/index.html";
-};
