0ea036a540174fb74a421f62e1dd5c69956c8d86
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-16 20:50:32 +0430

    [MOD] layout manager

--- src/app/dashboard/song-list/layout-manager/layout-manager.component.ts
+++ src/app/dashboard/song-list/layout-manager/layout-manager.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
-  selector: 'app-layout-manager',
-  templateUrl: './layout-manager.component.html',
-  styleUrls: ['./layout-manager.component.scss']
+    selector: 'app-layout-manager',
+    templateUrl: './layout-manager.component.html',
+    styleUrls: ['./layout-manager.component.scss'],
 })
 export class LayoutManagerComponent implements OnInit {
+    constructor() {}
+    @Output() chosedLayout = new EventEmitter<string>();
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    layoutChangeHandler(e: any) {
+        this.chosedLayout.emit(e.target.id);
+    }
 }
