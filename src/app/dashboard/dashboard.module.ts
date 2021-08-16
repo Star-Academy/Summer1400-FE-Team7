1401c93e3115cbb04a86b29c173615aa674d5286
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-16 20:51:29 +0430

    [MOD] .

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,17 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import {RouterModule} from "@angular/router";
-
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {RouterModule} from '@angular/router';
+import {SideMenuComponent} from './side-menu/side-menu.component';
+import {PlayControllersComponent} from './play-controllers/play-controllers.component';
+import {SongListComponent} from './song-list/song-list.component';
+import {LyricsComponent} from './lyrics/lyrics.component';
+import {MakeNewPlaylistComponent} from './make-new-playlist/make-new-playlist.component';
+import {LayoutManagerComponent} from './song-list/layout-manager/layout-manager.component';
+import {MenuItemComponent} from './side-menu/menu-item/menu-item.component';
+import {SearchComponent} from './side-menu/search/search.component';
+import {SongItemComponent} from './song-list/song-item/song-item.component';
+import {AddToPlaylistPanelComponent} from './add-to-playlist-panel/add-to-playlist-panel.component';
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ]
+    declarations: [
+        DashboardComponent,
+        SideMenuComponent,
+        PlayControllersComponent,
+        SongListComponent,
+        LyricsComponent,
+        MakeNewPlaylistComponent,
+        LayoutManagerComponent,
+        MenuItemComponent,
+        SearchComponent,
+        SongItemComponent,
+        AddToPlaylistPanelComponent,
+    ],
+    imports: [CommonModule, RouterModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
