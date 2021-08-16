1024d90201709f467b0b7529d1487a4ec5490a1c
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-16 20:51:03 +0430

    [MOD] dashboard

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {UiManagerService} from 'src/app/services/ui-manager.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+    public isCreateNewplaylistPanelOpen: Subscription = new Subscription();
+    public isPanelOpen: boolean = false;
 
-  constructor() { }
+    public isAddtoPlaylistPanelOpenSub: Subscription = new Subscription();
+    public isAddtoPlaylistPanelOpen: boolean = false;
 
-  ngOnInit(): void {
-  }
+    constructor(private uiManager: UiManagerService) {}
 
+    ngOnInit(): void {
+        this.isCreateNewplaylistPanelOpen = this.uiManager.isCreateNewplaylistPanelOpen.subscribe((state: boolean) => {
+            this.isPanelOpen = state;
+        });
+
+        this.isAddtoPlaylistPanelOpenSub = this.uiManager.isAddtoNewPlaylistPanelOpen.subscribe((state: boolean) => {
+            this.isAddtoPlaylistPanelOpen = state;
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.isCreateNewplaylistPanelOpen.unsubscribe();
+        this.isAddtoPlaylistPanelOpenSub.unsubscribe();
+    }
+
+    closeNewPlaylistPanel() {
+        this.uiManager.closeCreatePlaylistPanel();
+        this.uiManager.closeAddtoNewPlaylistPanel();
+    }
 }
