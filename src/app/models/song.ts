57e2c886e0ffedf831dce1fc5bd09680a79d1b99
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-16 17:22:09 +0430

    [FIX] song model

--- src/app/models/song.ts
+++ src/app/models/song.ts
@@ -1,17 +1,30 @@
 export class Song {
-    title!: string;
+    id!: number;
+    name!: string;
     artist!: string;
     duration?: string;
     cover!: string;
-    isFavourite!: boolean;
+    isFavourite?: boolean;
     lyrics!: string;
+    file!: string;
 
-    constructor(title: string, artist: string, duration: string, cover: string, isFavourite: boolean, lyrics: string) {
-        this.title = title;
+    constructor(
+        id: number,
+        name: string,
+        artist: string,
+        duration: string,
+        cover: string,
+        isFavourite: boolean,
+        lyrics: string,
+        file: string
+    ) {
+        this.id = id;
+        this.name = name;
         this.artist = artist;
         this.duration = duration;
         this.cover = cover;
         this.isFavourite = isFavourite;
         this.lyrics = lyrics;
+        this.file = file;
     }
 }
