17fb18c79ee535f7cec9e338ed3163f7d4fb4adf
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-16 12:05:10 +0430

    [ADD] lyrics component

---
+++ src/app/dashboard/lyrics/lyrics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lyrics',
+  templateUrl: './lyrics.component.html',
+  styleUrls: ['./lyrics.component.scss']
+})
+export class LyricsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
