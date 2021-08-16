7e1dea03d3171bbc59a4c9c429dbbe394a115f40
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-16 12:04:52 +0430

    [ADD] layout manager component

---
+++ src/app/dashboard/layout-manager/layout-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-manager',
+  templateUrl: './layout-manager.component.html',
+  styleUrls: ['./layout-manager.component.scss']
+})
+export class LayoutManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
