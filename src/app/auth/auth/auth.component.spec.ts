097c0b45274c16a134d128a4298321ccad344b49
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-17 14:21:13 +0430

    [ADD] test for auth url params

--- src/app/auth/auth/auth.component.spec.ts
+++ src/app/auth/auth/auth.component.spec.ts
@@ -1,14 +1,26 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {ActivatedRoute, RouterModule} from '@angular/router';
 import {AuthComponent} from './auth.component';
+import {of} from 'rxjs';
+import {RouterTestingModule} from '@angular/router/testing';
 
-describe('AuthComponent', () => {
+describe('AUTH PAGE login form', () => {
     let component: AuthComponent;
     let fixture: ComponentFixture<AuthComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [AuthComponent],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        queryParams: of({
+                            name: 'register',
+                        }),
+                    },
+                },
+            ],
         }).compileComponents();
     });
 
@@ -18,7 +30,40 @@ describe('AuthComponent', () => {
         fixture.detectChanges();
     });
 
-    // it('should create', () => {
-    //   expect(component).toBeTruthy();
-    // });
+    it('should fill __AUTH__ REGISTER form', () => {
+        console.log(component);
+        expect(component.isLoginPage).toBeFalsy();
+    });
+});
+
+describe('AUTH PAGE register form', () => {
+    let component: AuthComponent;
+    let fixture: ComponentFixture<AuthComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AuthComponent],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        queryParams: of({
+                            name: 'login',
+                        }),
+                    },
+                },
+            ],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AuthComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should fill __AUTH__ LOGIN form', () => {
+        console.log(component);
+        expect(component.isLoginPage).toBeTruthy();
+    });
 });
