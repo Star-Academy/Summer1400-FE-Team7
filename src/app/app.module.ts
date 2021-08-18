985c95873a64421d8952cb3506c19cf35c80e66b
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-18 16:37:07 +0430

    [MOD] .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
-import { LandingModule } from './landing/landing.module';
-import {DashboardModule} from "./dashboard/dashboard.module";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthModule} from './auth/auth.module';
+import {LandingModule} from './landing/landing.module';
+import {DashboardModule} from './dashboard/dashboard.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    FormsModule,
-    AuthModule,
-    LandingModule,
-    DashboardModule
-  ],
-  providers: [],
-  exports: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        FormsModule,
+        AuthModule,
+        LandingModule,
+        DashboardModule,
+    ],
+    providers: [],
+    exports: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
