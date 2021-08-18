3b6fec123eb8850dc67c2d3fda8176e1338650fa
milad nafar <milarnafar78+glab@gmail.com>
2021-08-18 12:02:04 +0430

    [ADD] constants file

---
+++ src/app/utils/constants.ts
@@ -0,0 +1,5 @@
+export class Constants{
+  static readonly ALL_SONGS: string="همه آهنگ ها"
+  static readonly FAVOURITE_SONGS: string="مورد علاقه"
+  static readonly ALL_PLAYLISTS: string="پلی لیست ها"
+}
