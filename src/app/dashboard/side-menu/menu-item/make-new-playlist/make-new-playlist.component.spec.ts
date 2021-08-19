1c2648af4f91c82173da440098a3f85de0d5871a
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-19 11:59:43 +0430

    [ADD] make-new-playlist component

---
+++ src/app/dashboard/side-menu/menu-item/make-new-playlist/make-new-playlist.component.spec.ts
@@ -0,0 +1,24 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {MakeNewPlaylistComponent} from './make-new-playlist.component';
+
+describe('MakeNewPlaylistComponent', () => {
+    let component: MakeNewPlaylistComponent;
+    let fixture: ComponentFixture<MakeNewPlaylistComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [MakeNewPlaylistComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MakeNewPlaylistComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    // it('should create', () => {
+    //   expect(component).toBeTruthy();
+    // });
+});
