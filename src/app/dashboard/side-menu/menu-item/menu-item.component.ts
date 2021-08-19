386253f2d27f1464823f05f44ab37a20499eee15
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-19 12:56:42 +0430

    [FIX] hover effect

--- src/app/dashboard/side-menu/menu-item/menu-item.component.ts
+++ src/app/dashboard/side-menu/menu-item/menu-item.component.ts
@@ -1,55 +1,51 @@
 import {Component, OnInit, Input, HostListener} from '@angular/core';
 import {Router} from '@angular/router';
 import {PlaylistItem} from 'src/app/models/playlistItem';
- import {SongService} from "../../../services/song.service";
+import {SongService} from '../../../services/song.service';
 
 @Component({
-  selector: 'app-menu-item',
-  templateUrl: './menu-item.component.html',
-  styleUrls: ['./menu-item.component.scss'],
+    selector: 'app-menu-item',
+    templateUrl: './menu-item.component.html',
+    styleUrls: ['./menu-item.component.scss'],
 })
 export class MenuItemComponent implements OnInit {
-  @Input() playlistItem!: PlaylistItem;
+    @Input() playlistItem!: PlaylistItem;
 
-  isPanelOpen: boolean = false;
-  showRemoveIcon: boolean = false;
+    isPanelOpen: boolean = false;
 
-  constructor( private songService: SongService,
-              private router: Router) {
-  }
+    constructor(private songService: SongService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
-  openNewPlaylistPanel() {
-    this.isPanelOpen = true;
-  }
+    openNewPlaylistPanel() {
+        this.isPanelOpen = true;
+    }
 
-  onRemove() {
-    this.songService.removePlaylist(this.playlistItem.id);
-  }
+    onRemove() {
+        this.songService.removePlaylist(this.playlistItem.id);
+    }
 
-  @HostListener('document:keydown', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-    if (event.key === "Escape")
-      if (this.isPanelOpen) {
-        this.isPanelOpen = false
-      }
-  }
-  // onPlaylistItemClick() {
-  //   console.log(this.playlistItem.name);
-  //   this.router.navigate(["/dashboard"],
-  //     {queryParams:{playlist:this.playlistItem.name}})
-  // }
-  closePanel() {
-    this.isPanelOpen=false;
-  }
+    @HostListener('document:keydown', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent) {
+        if (event.key === 'Escape')
+            if (this.isPanelOpen) {
+                this.isPanelOpen = false;
+            }
+    }
+    // onPlaylistItemClick() {
+    //   console.log(this.playlistItem.name);
+    //   this.router.navigate(["/dashboard"],
+    //     {queryParams:{playlist:this.playlistItem.name}})
+    // }
+    closePanel() {
+        this.isPanelOpen = false;
+    }
 
-  onMouseout() {
-this.showRemoveIcon=false;
-  }
+    // onMouseout() {
+    //     this.showRemoveIcon = false;
+    // }
 
-  onMouseover() {
-    this.showRemoveIcon=true  ;
-  }
+    // onMouseover() {
+    //     this.showRemoveIcon = true;
+    // }
 }
