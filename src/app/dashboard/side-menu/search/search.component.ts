d7723842b207eb8fa080a93e824f5917eaec2724
milad nafar <milarnafar78+glab@gmail.com>
2021-08-20 13:28:31 +0430

    [ADD] search deBounder and fix ui

--- src/app/dashboard/side-menu/search/search.component.ts
+++ src/app/dashboard/side-menu/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {debounceTime} from "rxjs/operators";
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  form: FormGroup = new FormGroup({
+    searchText: new FormControl()
+  });
+
+  obs!: Subscription;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.obs=this.form.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(data => this.performSearch(data.searchText));
+  }
+
+  ngOnDestroy() {
+    this.obs.unsubscribe();
+  }
+
+
+  performSearch(searchText:string){
+    console.log(searchText)
   }
 
 }
