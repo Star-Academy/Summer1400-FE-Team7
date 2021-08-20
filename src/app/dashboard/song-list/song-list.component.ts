1a572cfff72289ffb4e0da044611b7438dc9796c
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-08-20 17:58:06 +0430

    [ADD] handled layout change in mobile veersion

--- src/app/dashboard/song-list/song-list.component.ts
+++ src/app/dashboard/song-list/song-list.component.ts
@@ -2,44 +2,58 @@ import {Component, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {Song} from 'src/app/models/song';
 import {SongService} from 'src/app/services/song.service';
-import {Playlist} from "../../models/playlist";
+import {Playlist} from '../../models/playlist';
 
 @Component({
-  selector: 'app-song-list',
-  templateUrl: './song-list.component.html',
-  styleUrls: ['./song-list.component.scss'],
+    selector: 'app-song-list',
+    templateUrl: './song-list.component.html',
+    styleUrls: ['./song-list.component.scss'],
+    host: {
+        '(window:resize)': 'onResize($event)',
+    },
 })
 export class SongListComponent implements OnInit {
-  // allSongs: Song[] = [];
-  currentPlaylist: Song[] = [];
-  currentPlaylistName: string="";
+    // allSongs: Song[] = [];
+    currentPlaylist: Song[] = [];
+    currentPlaylistName: string = '';
 
-  // allSongsSub!: Subscription;
-  currentPlaylistSub!: Subscription;
+    // allSongsSub!: Subscription;
+    currentPlaylistSub!: Subscription;
 
-  layoutView: string = '';
+    layoutView: string = '';
 
-  constructor(private songService: SongService) {
-  }
+    windowWidht!: number;
 
-  ngOnInit(): void {
-    this.currentPlaylistSub = this.songService.currentPlaylistChanged.subscribe((data:Playlist) => {
-      this.currentPlaylistName = data.name;
-      this.currentPlaylist = data.songs;
-    });
+    constructor(private songService: SongService) {}
 
-    // this.allSongsSub = this.songService.allSongsChanged.subscribe((data: Song[]) => {
-    //     this.allSongs = data;
-    // });
-    // this.allSongs = this.songService.allSongs;
-  }
+    ngOnInit(): void {
+        this.currentPlaylistSub = this.songService.currentPlaylistChanged.subscribe((data: Playlist) => {
+            this.currentPlaylistName = data.name;
+            this.currentPlaylist = data.songs;
+        });
 
-  ngOnDestroy(): void {
-    // this.allSongsSub.unsubscribe();
-    this.currentPlaylistSub.unsubscribe();
-  }
+        // this.allSongsSub = this.songService.allSongsChanged.subscribe((data: Song[]) => {
+        //     this.allSongs = data;
+        // });
+        // this.allSongs = this.songService.allSongs;
+    }
 
-  changeLayoutViewHandler(viewType: any) {
-    this.layoutView = viewType;
-  }
+    onResize(event: any) {
+        //TODO
+        this.windowWidht = event.target.innerWidth;
+        if (this.windowWidht < 750) {
+            if (this.layoutView === 'list-compact-view') {
+                this.changeLayoutViewHandler('list-view');
+            }
+        }
+    }
+
+    ngOnDestroy(): void {
+        // this.allSongsSub.unsubscribe();
+        this.currentPlaylistSub.unsubscribe();
+    }
+
+    changeLayoutViewHandler(viewType: any) {
+        this.layoutView = viewType;
+    }
 }
