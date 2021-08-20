c1317cb3fe20258f492676f142ed0194b6947afc
milad nafar <milarnafar78+glab@gmail.com>
2021-08-20 17:39:09 +0430

    [ADD] like image Scale animation directive Added

---
+++ src/app/directives/like-hover.directive.spec.ts
@@ -0,0 +1,8 @@
+import { LikeHoverDirective } from './like-hover.directive';
+
+describe('LikeHoverDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LikeHoverDirective();
+    expect(directive).toBeTruthy();
+  });
+});
