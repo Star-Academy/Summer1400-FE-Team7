bc59b902a5c023ecbcf5e11714307644f6348636
milad nafar <milarnafar78+glab@gmail.com>
2021-08-20 13:27:45 +0430

    [ADD] lyrics data

--- src/app/dashboard/lyrics/lyrics.component.ts
+++ src/app/dashboard/lyrics/lyrics.component.ts
@@ -1,15 +1,35 @@
-import {Component, OnInit, EventEmitter, Output, Input} from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, Input, OnDestroy} from '@angular/core';
+import {SongService} from "../../services/song.service";
+import {Song} from "../../models/song";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-lyrics',
     templateUrl: './lyrics.component.html',
     styleUrls: ['./lyrics.component.scss'],
 })
-export class LyricsComponent implements OnInit {
+export class LyricsComponent implements OnInit ,OnDestroy{
     @Output() toggleLyric = new EventEmitter<void>();
     @Input() isPanelOpen!: boolean;
 
-    constructor() {}
+  playingSong: Song = new Song();
+  playingSongSub!: Subscription;
 
-    ngOnInit(): void {}
+    constructor(private songService: SongService) {}
+
+    ngOnInit(): void {
+       this.playingSongSub = this.songService.playingSongChange.subscribe((song: Song) => {
+        this.playingSong = song;
+         console.log(song.lyrics)
+      });
+    }
+
+    ngOnDestroy(): void {
+    this.playingSongSub.unsubscribe();
+    }
+
+  onBackClick() {
+    this.isPanelOpen=false;
+    this.toggleLyric.emit()
+  }
 }
