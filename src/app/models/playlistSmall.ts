6222c69092a983686fb214fd583090a00a3a537b
milad nafar <milarnafar78+glab@gmail.com>
2021-08-20 11:18:24 +0430

    [ADD] is checked property

--- src/app/models/playlistSmall.ts
+++ src/app/models/playlistSmall.ts
@@ -1,11 +1,11 @@
 export class PlaylistSmall {
     public id!: number;
     public name!: string;
-    public isSelected!: boolean;
+    public isChecked!: boolean;
 
-    constructor(id: number, name: string, isSelected: boolean) {
+    constructor(id: number, name: string, isChecked: boolean) {
         this.id = id;
         this.name = name;
-        this.isSelected = isSelected;
+        this.isChecked = isChecked;
     }
 }
