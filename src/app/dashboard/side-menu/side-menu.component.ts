a6266d874b6a6d35aadef3a22e381d7c9ae651ca
milad nafar <milarnafar78+glab@gmail.com>
2021-08-20 11:19:57 +0430

    [FIX] cleanup

--- src/app/dashboard/side-menu/side-menu.component.ts
+++ src/app/dashboard/side-menu/side-menu.component.ts
@@ -5,47 +5,50 @@ import {Subscription} from 'rxjs';
 import {Song} from '../../models/song';
 import {Playlist} from '../../models/playlist';
 import {Constants} from '../../utils/constants';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
-    selector: 'app-side-menu',
-    templateUrl: './side-menu.component.html',
-    styleUrls: ['./side-menu.component.scss'],
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.scss'],
+
 })
 export class SideMenuComponent implements OnInit {
-    public playlists!: PlaylistItem[];
-    private initialPlaylists: PlaylistItem[] = [
-        new PlaylistItem(Constants.ALL_SONGS, -1, '../../../../assets/images/playlist.svg', 'none', '', true),
-        new PlaylistItem(Constants.FAVOURITE_SONGS, -2, '../../../assets/images/favourite.svg', 'none', '', false),
-        new PlaylistItem(Constants.ALL_PLAYLISTS, -3, '../../../assets/images/playlist-add.svg', 'add', '', false),
-    ];
-    playlistsSub!: Subscription;
+  public playlists!: PlaylistItem[];
+  private initialPlaylists: PlaylistItem[] = [
+    new PlaylistItem(Constants.ALL_SONGS, -1, '../../../../assets/images/playlist.svg', 'none', '', true),
+    new PlaylistItem(Constants.FAVOURITE_SONGS, -2, '../../../assets/images/favourite.svg', 'none', '', false),
+    new PlaylistItem(Constants.ALL_PLAYLISTS, -3, '../../../assets/images/playlist-add.svg', 'add', '', false),
+  ];
+  playlistsSub!: Subscription;
 
-    @Input() public isOpen!: boolean;
+  @Input() public isOpen!: boolean;
 
-    constructor(private songService: SongService) {
-        this.playlistsSub = this.songService.allPlaylistsChanged.subscribe((data: Playlist[]) => {
-            this.playlists = [];
-            this.playlists = this.initialPlaylists.slice();
-            data.forEach((playlist, index) => {
-                if (playlist.name !== 'مورد علاقه') {
-                    this.playlists.push(
-                        new PlaylistItem(
-                            playlist.name,
-                            playlist.id,
-                            '../../../assets/images/new-playlist.svg',
-                            'remove',
-                            '',
-                            false
-                        )
-                    );
-                }
-            });
-        });
-    }
+  constructor(private songService: SongService) {
+    this.playlistsSub = this.songService.allPlaylistsChanged.subscribe((data: Playlist[]) => {
+      this.playlists = [];
+      this.playlists = this.initialPlaylists.slice();
+      data.forEach((playlist, index) => {
+        if (playlist.name !== 'مورد علاقه') {
+          this.playlists.push(
+            new PlaylistItem(
+              playlist.name,
+              playlist.id,
+              '../../../assets/images/new-playlist.svg',
+              'remove',
+              '',
+              false
+            )
+          );
+        }
+      });
+    });
+  }
 
-    ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
-    ngOnDestroy(): void {
-        this.playlistsSub.unsubscribe();
-    }
+  ngOnDestroy(): void {
+    this.playlistsSub.unsubscribe();
+  }
 }
