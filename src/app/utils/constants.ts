fd05c111c26d69fae9bbb13eceae000036660e7a
milad nafar <milarnafar78+glab@gmail.com>
2021-08-20 17:41:08 +0430

    [ADD] SEARCH constant

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -2,4 +2,5 @@ export class Constants{
   static readonly ALL_SONGS: string="همه آهنگ ها"
   static readonly FAVOURITE_SONGS: string="مورد علاقه"
   static readonly ALL_PLAYLISTS: string="پلی لیست ها"
+  static readonly SEARCH_SONGS: string="جستوجو"
 }
