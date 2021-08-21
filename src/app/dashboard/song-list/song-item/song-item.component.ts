b1b88f695279f863c04f5ef266f68b7712406d35
milad nafar <milarnafar78+glab@gmail.com>
2021-08-21 19:09:42 +0430

    [ADD] add play on song

--- src/app/dashboard/song-list/song-item/song-item.component.ts
+++ src/app/dashboard/song-list/song-item/song-item.component.ts
@@ -1,11 +1,13 @@
-import {Component, OnInit, Input, HostListener} from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import {Component, OnInit, Input, HostListener, OnDestroy} from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 import {Song} from 'src/app/models/song';
 import {SongService} from 'src/app/services/song.service';
 import {NotificationService} from 'src/app/services/notification.service';
-import { Constants } from 'src/app/utils/constants';
+import {Constants} from 'src/app/utils/constants';
 import {Playlist} from "../../../models/playlist";
+import {PlayControllerService} from 'src/app/services/play-controller.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-song-item',
@@ -18,45 +20,67 @@ import {Playlist} from "../../../models/playlist";
         transition(
           ':enter',
           [
-            style({  opacity: 0, transform: 'scale(0.8)',
-            transformOrigin:"center"}),
+            style({
+              opacity: 0, transform: 'scale(0.8)',
+              transformOrigin: "center"
+            }),
             animate('0.3s ease-out',
-              style({  opacity: 1 , transform: 'scale(1)',transformOrigin:"center"}))
+              style({opacity: 1, transform: 'scale(1)', transformOrigin: "center"}))
           ]
         ),
         transition(
           ':leave',
           [
-            style({  opacity: 1,transform: 'scale(1)',transformOrigin:"center" }),
+            style({opacity: 1, transform: 'scale(1)', transformOrigin: "center"}),
             animate('0.1s ease-in',
-              style({  opacity: 0 , transform: 'scale(0.8)',transformOrigin:"center"}))
+              style({opacity: 0, transform: 'scale(0.8)', transformOrigin: "center"}))
           ]
         )
       ]
     )
   ]
 })
-export class SongItemComponent implements OnInit {
+export class SongItemComponent implements OnInit, OnDestroy {
+  readonly statusTypes = {
+    PLAYING: 'playing',
+    PAUSED: 'paused',
+    STOPPED: 'stopped',
+    LOADING: 'loading',
+    MUTED: 'muted',
+  };
+
   @Input() song!: Song;
   @Input() index!: number;
   @Input() layout!: string;
-
+  isPlaying: boolean = false;
+  playingSongSub!: Subscription;
   canDelete: boolean = false;
-  currentPlaylist!:Playlist;
+  currentPlaylist!: Playlist;
+
+  status!:string;
+  statusSub!:Subscription;
 
   isMoreOptionsOpened: boolean = false;
   isAddToPlaylistPanelOpen: boolean = false;
 
-  constructor(private uiManager: NotificationService, private songService: SongService) {
+  constructor(private uiManager: NotificationService, private songService: SongService, private playController: PlayControllerService) {
   }
 
   ngOnInit(): void {
-    this.currentPlaylist=this.songService.currentPlaylist;
-    if(this.currentPlaylist.name!==Constants.ALL_SONGS &&
-      this.currentPlaylist.name!==Constants.FAVOURITE_SONGS ){
-      this.canDelete=true;
-      }
-   }
+    this.currentPlaylist = this.songService.currentPlaylist;
+    if (this.currentPlaylist.name !== Constants.ALL_SONGS &&
+      this.currentPlaylist.name !== Constants.FAVOURITE_SONGS) {
+      this.canDelete = true;
+    }
+    this.playingSongSub = this.songService.playingSongChange.subscribe((song: Song) => {
+      this.isPlaying = this.song.id === song.id
+    });
+    this.statusSub = this.playController.statusChanged.subscribe((status:string)=>{
+      this.status = status;
+    });
+
+
+  }
 
   openNewPlaylistPanel() {
     this.toggleNewPlaylistPanel()
@@ -71,7 +95,7 @@ export class SongItemComponent implements OnInit {
 
   onDoubleClick() {
     this.songService.playingSong = this.song;
-    this.songService.currenSongIndex=this.index;
+    this.songService.currenSongIndex = this.index;
   }
 
   onFavoriteClick() {
@@ -84,27 +108,49 @@ export class SongItemComponent implements OnInit {
   }
 
   toggleNewPlaylistPanel() {
-    this.isAddToPlaylistPanelOpen=!this.isAddToPlaylistPanelOpen;
+    this.isAddToPlaylistPanelOpen = !this.isAddToPlaylistPanelOpen;
   }
+
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
-    if (event.key==="Escape")
-    if (this.isAddToPlaylistPanelOpen){
-      this.isAddToPlaylistPanelOpen=false
-    }
+    if (event.key === "Escape")
+      if (this.isAddToPlaylistPanelOpen) {
+        this.isAddToPlaylistPanelOpen = false
+      }
 
 
   }
 
   onDeletePlaylist() {
-    this.songService.removeFromPlaylist(this.currentPlaylist.id,this.song.id)
-    this.isMoreOptionsOpened=false;
-    this.uiManager.showNotification("از پلی‌لیست حذف شد",false,()=>{
-     this.songService.addToPlaylist(this.currentPlaylist.id,this.song)
+    this.songService.removeFromPlaylist(this.currentPlaylist.id, this.song.id)
+    this.isMoreOptionsOpened = false;
+    this.uiManager.showNotification("از پلی‌لیست حذف شد", false, () => {
+      this.songService.addToPlaylist(this.currentPlaylist.id, this.song)
     })
   }
 
-  onPlayCover() {
-    console.log("Aaaaaaaaaa")
+  onPlaySong() {
+    if (!this.isPlaying){
+      if(this.songService.playingSong===undefined){
+        this.songService.playingSong = this.song;
+      }
+     else if(this.songService.playingSong.id === this.song.id){
+        this.playController.resume();
+        this.isPlaying=true;
+      }else{
+        this.songService.playingSong = this.song;
+      }
+    }else{
+      this.playController.pause();
+      this.isPlaying=false;
+    }
+
+
+  }
+
+
+  ngOnDestroy(): void {
+    this.playingSongSub.unsubscribe();
+    this.statusSub.unsubscribe()
   }
 }
