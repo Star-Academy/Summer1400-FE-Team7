2ad77b46975928673754b573090e6d87fd0f7fea
milad nafar <milarnafar78+glab@gmail.com>
2021-08-22 20:34:11 +0430

    [ADD] lyrics test

--- src/app/dashboard/lyrics/lyrics.component.spec.ts
+++ src/app/dashboard/lyrics/lyrics.component.spec.ts
@@ -1,28 +1,61 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import {LyricsComponent} from './lyrics.component';
 import {RouterTestingModule} from "@angular/router/testing";
+import {Song} from "../../models/song";
+import {SongService} from "../../services/song.service";
 
 describe('LyricsComponent', () => {
-    let component: LyricsComponent;
-    let fixture: ComponentFixture<LyricsComponent>;
+  let component: LyricsComponent;
+  let fixture: ComponentFixture<LyricsComponent>;
+  let songService: SongService
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [LyricsComponent],
-          imports: [  HttpClientTestingModule, RouterTestingModule],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LyricsComponent],
+      providers: [SongService],
+      imports: [HttpClientTestingModule, RouterTestingModule],
 
-        }).compileComponents();
-    });
+    }).compileComponents();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(LyricsComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LyricsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    songService = TestBed.get(SongService);
+  });
 
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
+  it('SongService should be injected with same instance',
+    inject([SongService], (injectService: SongService) => {
+      expect(injectService).toBe(songService);
+    })
+  );
+  it('playingSongSub should not be null after ngOnInit', () => {
+
+    let playingSongSub = component.playingSongSub;
+    component.ngOnInit();
+    expect(playingSongSub).not.toBeNull();
+
+  });
+
+
+  it('onBackClick() should make isPanelOpen false', () => {
+    let isPanelOpen = component.isPanelOpen;
+    component.onBackClick();
+    expect(isPanelOpen).toBeFalsy();
+
+  });
+
+  it('playingSong\'s id  should  be -1 ', () => {
+    let playingSong = component.playingSong;
+    expect(playingSong.id).toEqual(-1);
+
+  });
+
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
