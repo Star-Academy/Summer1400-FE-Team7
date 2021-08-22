399a2dde067f5e51c8b0f48d512b092aa6710202
milad nafar <milarnafar78+glab@gmail.com>
2021-08-22 20:34:22 +0430

    [ADD] notifications test

--- src/app/dashboard/dashboard/notification/notification.component.spec.ts
+++ src/app/dashboard/dashboard/notification/notification.component.spec.ts
@@ -1,14 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import { NotificationComponent } from './notification.component';
+
+import {NotificationService} from "../../../services/notification.service";
 
 describe('NotificationComponent', () => {
   let component: NotificationComponent;
   let fixture: ComponentFixture<NotificationComponent>;
+  let notificationService: NotificationService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NotificationComponent ]
+      declarations: [ NotificationComponent ],
+      providers:[NotificationService],
+
     })
     .compileComponents();
   });
@@ -16,8 +21,16 @@ describe('NotificationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NotificationComponent);
     component = fixture.componentInstance;
+    notificationService = TestBed.get(NotificationService);
+
     fixture.detectChanges();
   });
+  it('NotificationService should be injected with same instance',
+    inject([NotificationService], (injectService: NotificationService) => {
+      expect(injectService).toBe(notificationService);
+    })
+  );
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
